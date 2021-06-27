14ffbb9fd25a08801936dea11b25119179cf08f5
Shreyas <shreyas.ranjan@gmail.com>
2021-06-27 22:54:21 +0530

    Update KMP.c

--- KMP.c
+++ KMP.c
@@ -1,15 +1,20 @@
 
 https://leetcode.com/problems/implement-strstr/discuss/13153/KMP-Algorithm-using-C%2B%2B
 ---------------------------------------------------------------------------------
-n_len = n_str.size();
-vector<int> KMP(n_len, 0); // KMP: Knuth–Morris–Pratt Pattern Matching
-int i = 0;
-for(int j = 1; j < n_len; j++) {
-	while(i>0 && n_str[i] != n_str[j])
+vector<int> getKMP(string &str) {
+    int len = str.size();
+    vector<int> KMP(len, 0); // KMP: Knuth–Morris–Pratt Pattern Matching
+    int i = 0;
+        
+    for(int j = 1; j < len; j++) {
+        while(i>0 && str[i] != str[j])
             i = KMP[i-1];
-	if(n_str[i] == n_str[j])
+        if(str[i] == str[j])
             i++;
-	KMP[j] = i;
+        KMP[j] = i;
+    } // for
+        
+    return KMP;
 }
 ---------------------------------------------------------------------------------
  //KMP-algorithm:
