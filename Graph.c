9cfb738273f356cd4a8cab7a9895cb160937e06a
Shreyas <shreyas.ranjan@gmail.com>
2021-08-04 10:47:34 +0530

    Update Graph.c

--- Graph.c
+++ Graph.c
@@ -1,6 +1,7 @@
 
 # detect cycle in undirected graph
 https://github.com/mission-peace/interview/blob/master/python/graph/cycleundirectedgraph.py
+https://github.com/mission-peace/interview/blob/master/python/graph/cycledirectedgraph.py
 
 Problems:
   - https://leetcode.com/problems/alien-dictionary/
